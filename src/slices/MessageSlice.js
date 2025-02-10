f5d328f959651ecbadd75fef667aca3915571447
usuny0317 <usuny0317@naver.com>
2025-02-10 11:59:38 +0900

    rtk 설치에서 시간 오래걸림 명렁어 진짜 겨우 기억함

---
+++ src/slices/MessageSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+const MessageSlice = createSlice({
+  name: "message",
+  initialState,
+  reducers: {
+    //
+    setMessage: (state, action) => {
+      return (state = action.payload.state);
+    },
+  },
+});
+
+export const { setMessage } = MessageSlice.actions;
+export default MessageSlice.reducer;
